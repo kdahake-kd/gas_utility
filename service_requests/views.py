598c3032491fedec440a9d5cadef8e217c02c7e5
kiran-kd <kdahake412@gmail.com>
2024-05-28 14:16:50 +0530

    Your commit message

--- service_requests/views.py
+++ service_requests/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+git pull origin/master 
+git pull origin
+git push -u origin master
+git config pull.rebase false
+git pull origin master
+git config pull.rebase true 
+git pull origin master  
+git push -u origin master 
