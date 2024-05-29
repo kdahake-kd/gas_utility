29ada30778a80a073fd6144129395086cdb1df2c
Kiran Dahake <74830620+kdahake-kd@users.noreply.github.com>
2024-05-29 12:26:13 +0530

    Update views.py

--- service_requests/views.py
+++ service_requests/views.py
@@ -1,25 +1,4 @@
-# from rest_framework import viewsets
-# from rest_framework.permissions import IsAuthenticated
-# from django.contrib.auth import get_user_model
-# from .models import ServiceRequest, ServiceRequestStatus
-# from .serializers import ServiceRequestSerializer, ServiceRequestStatusSerializer
 
-# User = get_user_model()
-
-# class ServiceRequestViewSet(viewsets.ModelViewSet):
-#     permission_classes = [IsAuthenticated]
-#     queryset = ServiceRequest.objects.all()
-#     serializer_class = ServiceRequestSerializer
-
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-
-# class ServiceRequestStatusViewSet(viewsets.ModelViewSet):
-#     queryset = ServiceRequestStatus.objects.all()
-#     serializer_class = ServiceRequestStatusSerializer
-
-
-# service_requests/views.py
 from rest_framework import viewsets
 from .models import ServiceRequest
 from .serializers import ServiceRequestSerializer
